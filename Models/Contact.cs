6b132284328858338ead7bb4ad308b3453df3325
oneilmarcelo <oneil.marcelo@gmail.com>
2019-02-04 14:29:08 -0200

    Criando entidades na pasta Models

---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+namespace contact_api.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long ZipCode { get; set; }
+    }
+}
---
+++ Models/ContactType.cs
@@ -0,0 +1,8 @@
+namespace contact_api.Models
+{
+    public class ContactType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
